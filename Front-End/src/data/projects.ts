53b9eece57d7f4fee9fdf0193fcced371d01e1d2
kauanvinicius9 <kauan.francischini@aluno.senai.br>
2026-06-06 22:28:02 -0300

    fix projects

--- Front-End/src/data/projects.ts
+++ Front-End/src/data/projects.ts
@@ -7,34 +7,4 @@ export const projects: Projects[] = [
 		desc: "API de usuários com Spring-Boot e Java",
 		link: "https://github.com/kauanvinicius9/Spring-Boot",
 	},
-	{
-        id: "2",
-		title: "Livraria Digital",
-		desc: "Livraria com autenticação via JWT e consumo de API",
-		link: "https://github.com/kauanvinicius9/BookStore",
-	},
-	{
-        id: "3",
-		title: "Cidade Inteligente",
-		desc: "Projeto Full-Stack com autenticação via JWT",
-		link: "https://github.com/kauanvinicius9/IP-SmartCity",
-	},
-	{
-        id: "4",
-		title: "Banco Digital Mobile",
-		desc: "Aplicativo de banco mobile",
-		link: "https://github.com/kauanvinicius9/CrediTech.git",
-	},
-	{
-        id: "5",
-		title: "API Mobile",
-		desc: "API de tarefas em produção",
-		link: "https://github.com/kauanvinicius9/API-Tasks-Dart.git",
-	},
-	{
-        id: "6",
-		title: "Aplicação Mobile",
-		desc: "Gerenciador fictício de estabelecimentos próximos",
-		link: "https://github.com/kauanvinicius9/LOCLY.git",
-	}
 ];
