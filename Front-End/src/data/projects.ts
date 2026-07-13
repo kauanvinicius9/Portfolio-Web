d6de9efba6b86e42aafba034a7f1812c0b2fd9dc
Francischini Kauan (CaP/ETS2) <kauan.francischini@aluno.senai.br>
2026-07-13 13:13:24 -0300

    add iris

--- Front-End/src/data/projects.ts
+++ Front-End/src/data/projects.ts
@@ -2,21 +2,27 @@ import type { Projects } from "../types/projects";
 
 export const projects: Projects[] = [
 	{
-        id: "1",
+        id: "01",
 		title: "CRUD",
 		desc: "API de usuários realizado em Spring-Boot",
 		link: "https://github.com/kauanvinicius9/Spring-Boot",
 	},
 	{
-        id: "2",
+        id: "02",
 		title: "ARYA",
 		desc: "Conexões e colaborações com IA integrada",
 		link: "https://github.com/kauanvinicius9/ARYA",
 	},
 	{
-        id: "3",
+        id: "03",
 		title: "API",
 		desc: "API de usuários realizado em FastAPI",
 		link: "https://github.com/kauanvinicius9/Users-API",
+	},
+	{
+        id: "04",
+		title: "ÍRIS",
+		desc: "Inteligência Artificial utilizando Ollama e Llama 3.2",
+		link: "https://github.com/kauanvinicius9/IRIS",
 	},
 ];
