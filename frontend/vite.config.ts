7182315d3c1f7507f7be41125f954754da9ba472
Kauan Vinícius <kauan.francischini@aluno.senai.br>
2026-07-29 10:06:05 -0300

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ['kauanvinicius.onrender.com']
+    allowedHosts: true
   },
   preview: {
-    allowedHosts: ['kauanvinicius.onrender.com']
+    allowedHosts: true
   }
 })
