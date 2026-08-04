51b56c8cf4e3feeea63f92a74532c0e9005d2272
Francischini Kauan (CaP/ETS2) <frk7ca@bosch.com>
2026-08-04 11:44:10 -0300

    move certificates to data folder

--- frontend/src/data/projects.ts
+++ frontend/src/data/projects.ts
@@ -1,52 +1,52 @@
 import type { Projects } from "../types/projects";
 
 export const projects: Projects[] = [
-	{
-        id: "01",
-		title: "CRUD",
-		desc: "API de usuários realizado em Spring-Boot",
-		link: "https://github.com/kauanvinicius9/Spring-Boot",
-	},
-	{
-        id: "02",
-		title: "Inteligência Artificial",
-		desc: "Inteligência Artificial utilizando Ollama e Llama 3.2",
-		link: "https://github.com/kauanvinicius9/IRIS",
-	},
-	{
-        id: "03",
-		title: "Logística",
-		desc: "Gerenciamento de dados logísticos com dashboard interativo",
-		link: "https://github.com/kauanvinicius9/Log-BR",
-	},
-	{
-        id: "04",
-		title: "Clientes",
-		desc: "Dashboard executivo de vendas",
-		link: "https://github.com/kauanvinicius9/Dashboard",
-	},
-	{
-        id: "05",
-		title: "Velas",
-		desc: "Site de velas com integração Mercado Pago SDK",
-		link: "https://github.com/kauanvinicius9/Candles",
-	},
-	{
-        id: "06",
-		title: "Estoque",
-		desc: "Gerenciamento de estoque de ferramentas",
-		link: "https://github.com/kauanvinicius9/Stock",
-	},
-	{
-        id: "07",
-		title: "Jogos",
-		desc: "Colecionador de jogos favoritados",
-		link: "https://github.com/kauanvinicius9/GameVault",
-	},
-	{
-        id: "08",
-		title: "Visualização Geral",
-		desc: "Motor automatizado de máquinas de estado",
-		link: "https://github.com/kauanvinicius9/FlowStream-Engine",
-	}
+  {
+    id: "01",
+    title: "CRUD",
+    desc: "API de usuários realizado em Spring-Boot",
+    link: "https://github.com/kauanvinicius9/Spring-Boot",
+  },
+  {
+    id: "02",
+    title: "Inteligência Artificial",
+    desc: "Inteligência Artificial utilizando Ollama e Llama 3.2",
+    link: "https://github.com/kauanvinicius9/IRIS",
+  },
+  {
+    id: "03",
+    title: "Logística",
+    desc: "Gerenciamento de dados logísticos com dashboard interativo",
+    link: "https://github.com/kauanvinicius9/Log-BR",
+  },
+  {
+    id: "04",
+    title: "Clientes",
+    desc: "Dashboard executivo de vendas",
+    link: "https://github.com/kauanvinicius9/Dashboard",
+  },
+  {
+    id: "05",
+    title: "Velas",
+    desc: "Site de velas com integração Mercado Pago SDK",
+    link: "https://github.com/kauanvinicius9/Candles",
+  },
+  {
+    id: "06",
+    title: "Estoque",
+    desc: "Gerenciamento de estoque de ferramentas",
+    link: "https://github.com/kauanvinicius9/Stock",
+  },
+  {
+    id: "07",
+    title: "Jogos",
+    desc: "Colecionador de jogos favoritados",
+    link: "https://github.com/kauanvinicius9/GameVault",
+  },
+  {
+    id: "08",
+    title: "Visualização Geral",
+    desc: "Motor automatizado de máquinas de estado",
+    link: "https://github.com/kauanvinicius9/FlowStream-Engine",
+  },
 ];
