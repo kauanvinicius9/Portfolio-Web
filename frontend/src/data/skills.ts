90b8e78aa846b6adcaca45663123f0d528d40c1b
Kauan Vinícius <kauan.francischini@aluno.senai.br>
2026-09-02 08:45:39 -0300

    Update skills.ts

--- frontend/src/data/skills.ts
+++ frontend/src/data/skills.ts
@@ -1,1 +1,97 @@
+import type { Skills } from "../types/skills";
+
+export const skills: Skills[] = [
+  {
+    id: "01",
+    name: "Spring-Boot",
+  },
+  {
+    id: "02",
+    name: "MongoDB",
+  },
+  {
+    id: "03",
+    name: "Vue",
+  },
+  {
+    id: "04",
+    name: "Next",
+  },
+  {
+    id: "05",
+    name: "Nuxt",
+  },
+  {
+    id: "06",
+    name: "PostgreSQL",
+  },
+  {
+    id: "07",
+    name: "HTML",
+  },
+  {
+    id: "08",
+    name: "CSS",
+  },
+  {
+    id: "09",
+    name: "JavaScript",
+  },
+  {
+    id: "10",
+    name: "React",
+  },
+  {
+    id: "11",
+    name: "FastAPI",
+  },
+  {
+    id: "12",
+    name: "Python",
+  },
+  {
+    id: "13",
+    name: "SQLServer",
+  },
+  {
+    id: "14",
+    name: "KNIME",
+  },
+  {
+    id: "15",
+    name: "Power BI",
+  },
+  {
+    id: "16",
+    name: "TypeScript",
+  },
+  {
+    id: "17",
+    name: "Django",
+  },
+  {
+    id: "18",
+    name: "Power Automate",
+  },
+  {
+    id: "19",
+    name: "Docker",
+  },
+  {
+    id: "20",
+    name: "Bootstrap",
+  },
+  {
+    id: "21",
+    name: "Java",
+  },
+  {
+    id: "22",
+    name: "Angular",
+  },
+  {
+    id: "23",
+    name: "SCSS / SASS",
+  },
+];
 
