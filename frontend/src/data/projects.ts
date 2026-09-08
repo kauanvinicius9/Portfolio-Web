b7e28f46a537acdff6c3711c18b6e57a0f7a409d
Kauan Vinícius <kauan.francischini@aluno.senai.br>
2026-09-08 12:25:20 -0300

    Update projects.ts

--- frontend/src/data/projects.ts
+++ frontend/src/data/projects.ts
@@ -3,44 +3,50 @@ import type { Projects } from "../types/projects";
 export const projects: Projects[] = [
   {
     id: "01",
-    title: "CRUD",
+    title: "UserCore",
     desc: "API de usuários realizado em Spring-Boot",
     link: "https://github.com/kauanvinicius9/Spring-Boot",
   },
   {
     id: "02",
-    title: "Logística",
+    title: "Log-BR",
     desc: "Gerenciamento de dados logísticos com dashboard interativo",
     link: "https://github.com/kauanvinicius9/Log-BR",
   },
   {
     id: "03",
-    title: "Velas",
+    title: "Reviva Velas",
     desc: "Site de velas aromatizadas, difusores e home-sprays",
     link: "https://github.com/kauanvinicius9/Candles",
   },
   {
     id: "04",
-    title: "Estoque",
+    title: "Stock",
     desc: "Gerenciamento de estoque de ferramentas",
     link: "https://github.com/kauanvinicius9/Stock",
   },
   {
     id: "05",
-    title: "Jogos",
+    title: "GameVault",
     desc: "Colecionador de jogos favoritados",
     link: "https://github.com/kauanvinicius9/GameVault",
   },
   {
     id: "06",
-    title: "Visualização Geral",
+    title: "FlowStream Engine",
     desc: "Motor automatizado de máquinas de estado",
     link: "https://github.com/kauanvinicius9/FlowStream-Engine",
   },
   {
     id: "07",
-    title: "Livraria",
+    title: "SPINNE",
     desc: "Gerenciamento de livros, editoras e autores",
     link: "https://github.com/kauanvinicius9/SPINNE",
+  },
+  {
+    id: "08",
+    title: "INVEX",
+    desc: "Simulador de investimentos e acompanhamento financeiro",
+    link: "https://github.com/kauanvinicius9/INVEX",
   },
 ];
