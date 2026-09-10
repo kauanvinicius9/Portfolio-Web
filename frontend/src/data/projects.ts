008d40b109bf1d4b4b79864dd93bce7b9ff0ee03
Kauan Vinícius <kauan.francischini@aluno.senai.br>
2026-09-10 15:25:37 -0300

    Add "Descriptions" to projects.ts

--- frontend/src/data/projects.ts
+++ frontend/src/data/projects.ts
@@ -6,47 +6,55 @@ export const projects: Projects[] = [
     title: "UserCore",
     desc: "API de usuários realizado em Spring-Boot",
     link: "https://github.com/kauanvinicius9/Spring-Boot",
+    efficiency: "Desenvolvido para melhorar o gerenciamento de informações para o seu negócio. Seja para usuários, produtos ou demais dados. Acompanhamento e manutenção com melhor eficiência",
   },
   {
     id: "02",
     title: "Log-BR",
     desc: "Gerenciamento de dados logísticos com dashboard interativo",
     link: "https://github.com/kauanvinicius9/Log-BR",
+    efficiency: "Desenvolvido para melhorar significativamente a descentralização de dados e o caos das planilha, resolvendo dependência de processos manuais",
   },
   {
     id: "03",
     title: "Reviva Velas",
     desc: "Site de velas aromatizadas, difusores e home-sprays",
     link: "https://github.com/kauanvinicius9/Candles",
+    efficiency: "Desenvolvido para alavancar vendas reais de produtos artesanais utilizando uma vitrine web responsiva e intuitiva",
   },
   {
     id: "04",
     title: "Stock",
     desc: "Gerenciamento de estoque de ferramentas",
     link: "https://github.com/kauanvinicius9/Stock",
+    efficiency: "Desenvolvido para acompanhamento de estoque virtual de ferramentas, diminuindo a desorganização e a ausência de controle",
   },
   {
     id: "05",
     title: "GameVault",
     desc: "Colecionador de jogos favoritados",
     link: "https://github.com/kauanvinicius9/GameVault",
+    efficiency: "Desenvolvido para gerenciamento e favoritagem de jogos de sua preferência. Esse sistema não resolve um problema real",
   },
   {
     id: "06",
     title: "FlowStream Engine",
     desc: "Motor automatizado de máquinas de estado",
     link: "https://github.com/kauanvinicius9/FlowStream-Engine",
+    efficiency: "Desenvolvido para diminuir o controle manual de fluxos complexos usando condicionais extensas. O sistema passa de um estado a outro apenas quando regras estritas forem atendidas"
   },
   {
     id: "07",
     title: "Spinne",
     desc: "Gerenciamento de livros, editoras e autores",
     link: "https://github.com/kauanvinicius9/Spinne",
+    efficiency: "Desenvolvido para gerenciamento de livros incluindo seus editores e suas autoras, podendo ser aplicada em uma livraria real"
   },
   {
     id: "08",
     title: "Invex",
     desc: "Simulador de investimentos e acompanhamento financeiro",
     link: "https://github.com/kauanvinicius9/Invex",
+    efficiency: "Desenvolvido para simulação real de investimentos com visualizações de dados através de dashboards, podendo melhorar sua organização financeira de forma eficiênte e simples"
   },
 ];
