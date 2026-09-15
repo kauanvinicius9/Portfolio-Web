26a2cdb93d2ae2b69fe01079d804261dbcab2cef
Kauan Vinícius <kauan.francischini@aluno.senai.br>
2026-09-15 08:23:05 -0300

    skills.ts

--- frontend/src/data/skills.ts
+++ frontend/src/data/skills.ts
@@ -2,100 +2,124 @@ import type { Skills } from "../types/skills";
 
 export const skills: Skills[] = [
   {
-    id: "01",
+    id: 1,
     name: "Spring-Boot",
+    category: "Backend",
   },
   {
-    id: "02",
+    id: 2,
     name: "MongoDB",
+    category: "Dados",
   },
   {
-    id: "03",
+    id: 3,
     name: "Vue",
+    category: "Frontend",
   },
   {
-    id: "04",
+    id: 04,
     name: "Next",
+    category: "Frontend",
   },
   {
-    id: "05",
+    id: 5,
     name: "Nuxt",
+    category: "Frontend",
   },
   {
-    id: "06",
+    id: 6,
     name: "PostgreSQL",
+    category: "Dados",
   },
   {
-    id: "07",
+    id: 7,
     name: "HTML",
+    category: "Frontend",
   },
   {
-    id: "08",
+    id: 8,
     name: "CSS",
+    category: "Frontend",
   },
   {
-    id: "09",
+    id: 9,
     name: "JavaScript",
+    category: "Frontend",
   },
   {
-    id: "10",
+    id: 10,
     name: "React",
+    category: "Frontend",
   },
   {
-    id: "11",
+    id: 11,
     name: "FastAPI",
+    category: "Backend",
   },
   {
-    id: "12",
+    id: 12,
     name: "Python",
+    category: "Backend",
   },
   {
-    id: "13",
+    id: 13,
     name: "SQLServer",
+    category: "Dados",
   },
   {
-    id: "14",
+    id: 14,
     name: "KNIME",
+    category: "Automação",
   },
   {
-    id: "15",
+    id: 15,
     name: "Power BI",
+    category: "Dados",
   },
   {
-    id: "16",
+    id: 16,
     name: "TypeScript",
+    category: "Frontend",
   },
   {
-    id: "17",
+    id: 17,
     name: "Django",
+    category: "Backend",
   },
   {
-    id: "18",
+    id: 18,
     name: "Power Automate",
+    category: "Automação",
   },
   {
-    id: "19",
+    id: 19,
     name: "Docker",
+    category: "Infra",
   },
   {
-    id: "20",
+    id: 20,
     name: "Bootstrap",
+    category: "Frontend",
   },
   {
-    id: "21",
+    id: 21,
     name: "Java",
+    category: "Backend",
   },
   {
-    id: "22",
+    id: 22,
     name: "Angular",
+    category: "Frontend",
   },
   {
-    id: "23",
+    id: 23,
     name: "SCSS / SASS",
+    category: "Frontend",
   },
   {
-    id: "24",
+    id: 24,
     name: "N8N",
+    category: "Automação",
   },
 ];
 
